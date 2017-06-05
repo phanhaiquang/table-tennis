8e2a26c7f3fe4773297306433b02684ae80bc6f2
quang <phanhaiquang@gmail.com>
2017-06-05 11:47:46 +0700

    fix conflict port at heroku

--- bin/start.sh
+++ bin/start.sh
@@ -3,5 +3,5 @@
 # https://medium.com/@fbzga/how-to-cache-bundle-install-with-docker-7bed453a5800#.h7fccyx1o
 bundle check || bundle install
 
-bundle exec foreman start -f Procfile
+bundle exec foreman start -f Procfile.dev
 
